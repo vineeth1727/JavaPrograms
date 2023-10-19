e5f96d928856581d4d3568a64937964dc9066c7e
vineeth1727 <146938755+vineeth1727@users.noreply.github.com>
2023-10-19 14:33:27 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,17 @@
+package One;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		int num=19;
+		int start=1;
+		int end=10;
+		
+		while(start<=end) {
+			System.out.println(num+"x"+start+"="+(num*start));
+			start++;
+		}
+
+	}
+
+}
