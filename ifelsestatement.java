c0f0ea3bfbff8f079024ebb292eb2dd1fa9fa01a
vineeth1727 <146938755+vineeth1727@users.noreply.github.com>
2023-10-19 10:18:22 +0530

    Add files via upload

---
+++ ifelsestatement.java
@@ -0,0 +1,15 @@
+package One;
+
+public class ifelsestatement {
+
+	public static void main(String[] args) {
+		
+		int num=87;
+		if(num%2==0) {
+			System.out.println("Given num is even :"+ num);
+		}else {
+			System.out.println("Given num is odd :"+ num);
+		}
+	}
+
+}
