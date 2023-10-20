b5d01e5034cc187d0a0c72f38b68fcb8cb1bac41
vineeth1727 <146938755+vineeth1727@users.noreply.github.com>
2023-10-20 16:48:36 +0530

    Add files via upload

---
+++ Equilateraltriangle.java
@@ -0,0 +1,17 @@
+package One;
+
+public class Equilateraltriangle {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=20;
+		int c=60;
+		int sum=a+b+c;
+		
+		if(sum==180) {
+			System.out.println("Given triangle is Equilateraltriangle");
+		}else
+			System.out.println("Given triangle is not Equilateraltriangle");
+	}
+
+}
